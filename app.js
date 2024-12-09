acd44477263e4cea7060504cf8067780b5538478
eq38770 <pallapothu.bhavana@ibm.com>
2024-12-09 13:19:39 +0530

    rendering  react element

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const heading=<h1 className="root">Namaste React</h1>
-const root=ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading);
-root.render(heading);
+const HeaderComponent=()=>(
+     <div><h1>This is Header Component</h1></div>
+);
 
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent/>);
